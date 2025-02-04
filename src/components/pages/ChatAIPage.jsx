f566a858b07e330eae8a62d5904eaa32ccdaf161
Yogawan Aditya Pratama <yogawanadityapratama@gmail.com>
2025-02-04 15:52:13 +0700

    Fix bg image issue

--- src/components/pages/ChatAIPage.jsx
+++ src/components/pages/ChatAIPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import JawirAIOrganism from "../organisms/JawirAIOrganism";
-import FooterOrganism from "../organisms/FooterOrganism";
-import CreatorOrganism from "../organisms/CreatorOrganism";
+// import FooterOrganism from "../organisms/FooterOrganism";
+// import CreatorOrganism from "../organisms/CreatorOrganism";
 
 const ChatAIPage = () => {
     return (
-        <div className="bg-[url('./assets/bg.png')] bg-cover bg-center h-screen">
+        <div className="bg-[url('/assets/bg.png')] bg-cover bg-center h-screen">
             {/* <CreatorOrganism /> */}
             <JawirAIOrganism />
             {/* <FooterOrganism /> */}
